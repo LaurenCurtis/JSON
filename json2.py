d5cd198c3832015d7c7380137ef776a46ebe6197
Lauren <you@example.com>
2021-10-06 16:34:21 -0500

    colormap

--- json2.py
+++ json2.py
@@ -10,9 +10,8 @@ json.dump(eqdata,outfile,indent = 4)
 print(eqdata["features"][0]["properties"]["mag"])
 
 list_of_eqs = eqdata["features"]
-mags = []
-long = []
-lat = []
+
+mags, lat, long, hover_text = [],[],[],[]
 for row in list_of_eqs:
     mag = row["properties"]["mag"]
     mags.append(mag)
@@ -20,6 +19,8 @@ for row in list_of_eqs:
     long.append(longtitude)
     latitude = row["geometry"]["coordinates"][1]
     lat.append(latitude)
+    text = row["properties"]["title"]
+    hover_text.append(text)
 
 #list slicing
 print(mags[0:5])
@@ -29,8 +30,21 @@ print(lat[:5])
 from plotly.graph_objs import Scattergeo, Layout 
 from plotly import offline
 
-
-data = [Scattergeo(lon = long, lat = lat)]
+#minimum needed
+#data = [Scattergeo(lon = long, lat = lat)]
+data = [{'type':'scattergeo',
+ 'lon': long,
+ 'lat': lat,
+ 'text': hover_text,
+ 'marker':{
+     'size': [5*m for m in mags],
+     'color': mags,
+     'colorscale': 'Viridis',
+     'reversescale': True,
+     'colorbar': {'title':'Magnitude'}
+ }
+}
+]
 my_layout = Layout(title = "Global Earthquakes 30 Day")
 fig = {'data':data, 'layout': my_layout}
 offline.plot(fig, filename = "globalearthquakesday30.html" )
